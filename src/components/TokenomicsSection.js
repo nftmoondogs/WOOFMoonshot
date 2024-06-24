f66587d325a1727e8d2265904bf9fbb239bfff40
nftmoondogs <nftmoondogs@gmail.com>
2024-06-25 05:19:58 +0530

    first commit

---
+++ src/components/TokenomicsSection.js
@@ -0,0 +1,25 @@
+// components/TokenomicsSection.js
+import React from 'react';
+
+const TokenomicsSection = () => {
+  return (
+    <div id="tokenomics" className="bg-gradient-to-b from-black to-purple-900 text-white py-12 px-4">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-start">
+        
+        {/* Left Section */}
+        <div className="flex-1 space-y-6">
+
+
+          <div>
+
+</div>
+        </div>
+        
+        {/* Right Section - Jupiter Terminal */}
+      
+      </div>
+    </div>
+  );
+};
+
+export default TokenomicsSection;
